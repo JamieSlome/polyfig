438f8c9c6b239fd89b5fbcdcc3f79b57b6576c66
JamieSlome <jamie@418sec.com>
2020-12-23 09:21:33 +0000

    fix(nuxt): adding polyfig/docs to base of the router

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     target: "static",
 
     router: {
-        base: "/polyfig/"
+        base: "/polyfig/docs/"
     },
 
     // Global page headers (https://go.nuxtjs.dev/config-head)
