788bbc9a35b53e73bc575022c6a3af2f72c3a8af
JamieSlome <jamie@418sec.com>
2020-12-23 09:25:56 +0000

    build(nuxt): revert router to polyfig base

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
     target: "static",
 
     router: {
-        base: "/polyfig/docs/"
+        base: "/polyfig/"
     },
 
     // Global page headers (https://go.nuxtjs.dev/config-head)
